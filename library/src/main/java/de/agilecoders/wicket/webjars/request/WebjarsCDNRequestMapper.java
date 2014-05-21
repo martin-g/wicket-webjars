3bcae96369c974a4dd0eced8aba155e0217f6e36
Andrew Geery <andrew.geery@gmail.com>
2014-05-20 21:02:08 -0400

    Modified the WebjarsCDNRequestMapper to remove the resource version using the undecorate method from the application's cache info -- based on how similar functionality is implemented in ResourceMapper.

--- library/src/main/java/de/agilecoders/wicket/webjars/request/WebjarsCDNRequestMapper.java
+++ library/src/main/java/de/agilecoders/wicket/webjars/request/WebjarsCDNRequestMapper.java
@@ -1,25 +1,29 @@
 package de.agilecoders.wicket.webjars.request;
 
-import java.util.regex.Pattern;
+import java.util.List;
 
+import org.apache.wicket.Application;
 import org.apache.wicket.request.IRequestHandler;
 import org.apache.wicket.request.IRequestMapper;
 import org.apache.wicket.request.Request;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.request.handler.resource.ResourceReferenceRequestHandler;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.ResourceReference;
+import org.apache.wicket.request.resource.caching.ResourceUrl;
 import org.apache.wicket.util.string.Strings;
 
 import de.agilecoders.wicket.webjars.request.resource.IWebjarsResourceReference;
 
 /**
- * Maps {@link ResourceReference}s of type {@link IWebjarsResourceReference} to the WebJar CDN URLs.
- * Based on de.agilecoders.wicket.extensions.request.StaticResourceRewriteMapper.
+ * Maps {@link ResourceReference}s of type {@link IWebjarsResourceReference} to
+ * the WebJar CDN URLs. Based on
+ * de.agilecoders.wicket.extensions.request.StaticResourceRewriteMapper.
  */
 public class WebjarsCDNRequestMapper implements IRequestMapper {
 
 	private static final String WEBJAR_PATH = "/webjars/";
-	
+
 	private final IRequestMapper chain;
 	private final String webJarCdnUrl;
 
@@ -37,11 +41,9 @@ public class WebjarsCDNRequestMapper implements IRequestMapper {
 					.getResourceReference();
 			if (resourceReference instanceof IWebjarsResourceReference) {
 				final Url url = chain.mapHandler(requestHandler);
-				String urlString = url.toString();
+				final String urlString = urlToStringWithNoVersion(url);
 				final int index = urlString.indexOf(WEBJAR_PATH);
 				if (index >= 0) {
-					// remove the version info from the url -- doesn't affect the webjars index above
-					urlString = url.toString().replaceAll("-ver-\\d+", "");
 					return Url.parse(Strings.join("/", webJarCdnUrl,
 							urlString.substring(index + WEBJAR_PATH.length())));
 				} else {
@@ -60,6 +62,32 @@ public class WebjarsCDNRequestMapper implements IRequestMapper {
 	@Override
 	public int getCompatibilityScore(final Request request) {
 		return 0;
+	}
+
+	/**
+	 * @return the string representation of the {@link Url} with any version
+	 *         info removed
+	 */
+	private static String urlToStringWithNoVersion(final Url url) {
+		final Url copy = new Url(url);
+		final List<String> segments = copy.getSegments();
+		if (!segments.isEmpty()) {
+			final int lastSegmentIndex = segments.size() - 1;
+			final String filename = segments.get(lastSegmentIndex);
+			if (!Strings.isEmpty(filename)) {
+				final ResourceUrl resourceUrl = new ResourceUrl(filename,
+						new PageParameters());
+				Application.get().getResourceSettings().getCachingStrategy()
+						.undecorateUrl(resourceUrl);
+				if (Strings.isEmpty(resourceUrl.getFileName())) {
+					throw new IllegalStateException(
+							"caching strategy returned empty name for "
+									+ resourceUrl);
+				}
+				segments.set(lastSegmentIndex, resourceUrl.getFileName());
+			}
+		}
+		return copy.toString();
 	}
 
 }
