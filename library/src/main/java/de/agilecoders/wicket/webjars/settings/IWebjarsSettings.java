7ead32691aebca16b9c153aacbb1a106a04bd259
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2016-03-27 13:13:58 +0200

    Revert "#41: allow sourcemaps in development mode by default"
    This reverts commit 4ea8b425806987ec071b76eb015f00e3abb07c08.

--- library/src/main/java/de/agilecoders/wicket/webjars/settings/IWebjarsSettings.java
+++ library/src/main/java/de/agilecoders/wicket/webjars/settings/IWebjarsSettings.java
@@ -9,7 +9,7 @@ import de.agilecoders.wicket.webjars.collectors.AssetPathCollector;
 /**
  * Settings interface for all webjars depended settings
  *
- * @author Michael Haitz <michael.haitz@agilecoders.de>
+ * @author miha
  */
 public interface IWebjarsSettings {
 
@@ -63,11 +63,6 @@ public interface IWebjarsSettings {
      * @return true, if the resources for the webjars should be loaded from a CDN network
      */
     boolean useCdnResources();
-
-    /**
-     * @return true, if sourcemaps are active
-     */
-    boolean sourcemaps();
     
     /**
      * @return base URL of the webjars CDN
