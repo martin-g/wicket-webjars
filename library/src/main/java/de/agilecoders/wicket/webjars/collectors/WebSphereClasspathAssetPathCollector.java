1cc470429f2a675d3d8447df4e278e86e60f65df
heryqk <eric.hamel@me.com>
2016-07-19 14:48:14 -0400

    WebSphere support for WicketWebjars

---
+++ library/src/main/java/de/agilecoders/wicket/webjars/collectors/WebSphereClasspathAssetPathCollector.java
@@ -0,0 +1,72 @@
+package de.agilecoders.wicket.webjars.collectors;
+
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.regex.Pattern;
+
+import de.agilecoders.wicket.webjars.settings.IWebjarsSettings;
+import edu.emory.mathcs.util.classloader.jar.JarProxy;
+
+/**
+ * A collector that searches for assets in the classpath, only in
+ * {@link IWebjarsSettings#webjarsPath()}, usually in <em>META-INF/resources/webjars/**</em>.
+ */
+public class WebSphereClasspathAssetPathCollector implements AssetPathCollector {
+
+	@Override
+	public boolean accept(final URL url) {
+		return true;
+	}
+
+    @Override
+    public Collection<String> collect(final URL url, final Pattern filterExpr) {
+        final Set<String> assetPaths = new HashSet<String>();
+        try {
+            Set<String> paths = collectFromWebJarPath(url, filterExpr);
+            assetPaths.addAll(paths);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        return assetPaths;
+    }
+
+    private Set<String> collectFromWebJarPath(URL webJarPathResource, final Pattern filterExpr) throws IOException {
+        final Set<String> assetPaths = new HashSet<String>();
+
+//        URLConnection urlConnection = webJarPathResource.openConnection();
+        
+        edu.emory.mathcs.util.classloader.jar.JarURLConnection urlConnection = new edu.emory.mathcs.util.classloader.jar.JarURLConnection(webJarPathResource, new JarProxy());
+        
+        if (urlConnection instanceof JarURLConnection) {
+            JarURLConnection urlcon = (JarURLConnection) urlConnection;
+            JarFile jar = null;
+            try {
+                jar = urlcon.getJarFile();
+                Enumeration<JarEntry> entries = jar.entries();
+                while (entries.hasMoreElements()) {
+                    String innerJarEntryName = entries.nextElement().getName();
+                    if (!isDirectory(innerJarEntryName) && filterExpr.matcher(innerJarEntryName).matches()) {
+                        assetPaths.add(innerJarEntryName);
+                    }
+                }
+            } finally {
+                if (jar != null) {
+                    jar.close();
+                }
+            }
+        }
+        return assetPaths;
+    }
+
+	private boolean isDirectory(String innerJarEntryName) {
+		return innerJarEntryName.endsWith("/");
+	}
+}
